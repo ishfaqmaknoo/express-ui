38162775e694f9db3811140be898a7bd23f0ec82
ishfaqmaknoo <ishfaqmaknoo12@gmail.com>
2025-05-18 14:50:50 +0530

    fix: add form data

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,58 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const app = document.querySelector<HTMLDivElement>("#app");
+if (!app) {
+  throw new Error("Not able to load the form");
+}
+
+app.innerHTML = `
+  <form id="register">
+    <div class="form">
+      <input type="text" name="name" placeholder="Enter name" />
+      <input type="email" name="email" placeholder="Enter name" />
+      <input type="password" name="password" placeholder="Enter name" />
+      <button>Submit</button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  </form>
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const registerForm = document.getElementById(
+  "register"
+) as HTMLFormElement | null;
+
+registerForm?.addEventListener("submit", registerUser);
+
+type FormValues = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+function registerUser(event: Event): void {
+  event.preventDefault();
+
+  const form = event.target as HTMLFormElement;
+  const formData = new FormData(form);
+
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const formValue = { name, email, password };
+
+  console.log("Form submitted:", formValue);
+
+  listUser(formValue);
+}
+
+function listUser(formValue: FormValues) {
+  app!.innerHTML += `
+    <div class="card">
+      <ul>
+        <li>${formValue.name}</li>
+        <li>${formValue.email}</li>
+        <li>${formValue.password}</li>
+      </ul>
+    </div>
+  `;
+}
